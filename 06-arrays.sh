577dafcb6d677da8175e4656a6fd837726fee85d
priya <haripriya.godishala12@gmail.com>
2024-08-30 18:38:03 +0530

    file 6

--- 06-arrays.sh
+++ 06-arrays.sh
@@ -3,3 +3,8 @@
 Dry_fruits=("Almonds" "Walnuts" "Raisins" "Anjeer")
 
 echo "1st dryfruit is : ${Dry_fruits[0]}"
+echo "2nd dryfruit is : ${Dry_fruits[1]}"
+echo "3rd dryfruit is : ${Dry_fruits[2]}"
+echo "4th dryfruit is : ${Dry_fruits[3]}"
+
+echo "all dryfruits are : ${Dry_fruits[@]}"

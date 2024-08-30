0751caead44013958c4425d32e9323fb77a62601
priya <haripriya.godishala12@gmail.com>
2024-08-30 18:43:15 +0530

    file 6

--- 06-arrays.sh
+++ 06-arrays.sh
@@ -5,7 +5,7 @@ NO_OF_FRUITS=$#
 
 echo "1st dryfruit is : ${Dry_fruits[$0]}"
 echo "2nd dryfruit is : ${Dry_fruits[$1]}"
-echo "3rd dryfruit is : ${Dry_fruits[2]}"
-echo "4th dryfruit is : ${Dry_fruits[3]}"
 
-echo "all dryfruits are : ${Dry_fruits[@]}"
+echo "4th dryfruit is : ${Dry_fruits[$3]}"
+
+#echo "all dryfruits are : ${Dry_fruits[@]}"

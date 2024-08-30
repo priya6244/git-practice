36886c4b2afe5b6e103ec06ebcbff45b38ecc0f4
priya <haripriya.godishala12@gmail.com>
2024-08-30 19:01:23 +0530

    file 6

--- 06-arrays.sh
+++ 06-arrays.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
 Dry_fruits=("Almonds" "Walnuts" "Raisins" "Anjeer")
-NO_OF_FRUITS=$#
+# #NO_OF_FRUITS=$#
 
-echo "1st dryfruit is : ${Dry_fruits[$0]}"
-echo "2nd dryfruit is : ${Dry_fruits[$1]}"
+echo "1st dryfruit is : ${Dry_fruits[0]}"
+echo "2nd dryfruit is : ${Dry_fruits[1]}"
 
-echo "4th dryfruit is : ${Dry_fruits[$3]}"
-echo " total no of fruits : ${NO_OF_FRUITS[$#]}"
+echo "all dryfruits are : ${Dry_fruits[@]}"
+
+# echo "4th dryfruit is : ${Dry_fruits[$3]}"
+# echo " total no of fruits : ${NO_OF_FRUITS[$#]}"
 
 #echo "all dryfruits are : ${Dry_fruits[@]}"

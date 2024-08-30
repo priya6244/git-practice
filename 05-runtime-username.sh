3a6b5887a0c6351d66241aae4417a688de03e511
priya <haripriya.godishala12@gmail.com>
2024-08-30 17:57:27 +0530

    file 5

--- 05-runtime-username.sh
+++ 05-runtime-username.sh
@@ -2,6 +2,9 @@
 
 echo "Please enter your username : "
 
-read USERNAME
+read USERNAME #interrupts and takes input from user and whatever the user enters it is visible to us
+
+read -s PASSWORD #whatever user enters here is invisible to us, becoz we have used -s here.
 
 echo "Username entered is : $USERNAME "
+echo "Password entered is : $PASSWORD "

6439383c977d72a80fd742ba38f6cfe4aa59b04d
priya <haripriya.godishala12@gmail.com>
2024-08-31 10:12:54 +0530

    file

--- 09-special-vars.sh
+++ 09-special-vars.sh
@@ -3,7 +3,7 @@
 echo "all variables : $@"
 echo "number of variables : $#"
 echo "script name : $0"
-echo "current working directory : $pwd"
+echo "current working directory : $PWD"
 echo "home directory : $home"
 echo "PID of the script executing now : $$"
 echo "PID of the last background command : $!"

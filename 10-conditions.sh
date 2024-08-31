8a108a98df1f032e94aee71edc7fffd995eb9947
priya <haripriya.godishala12@gmail.com>
2024-08-31 10:23:09 +0530

    file

--- 10-conditions.sh
+++ 10-conditions.sh
@@ -3,7 +3,16 @@
 #script to know the entered number is greater than 20 or not
 
 NUMBER=$1
-if [ $NUMBER -gt 20 ]
+# if [ $NUMBER -gt 20 ]
+# then
+#     echo "$NUMBER is greater than 20"
+# else
+#     echo "$NUMBER is less than 20"
+# fi
+
+#write a script to know the given number is greater than or equal to 20
+
+if [ $NUMBER -ge 20 ]
 then
     echo "$NUMBER is greater than 20"
 else

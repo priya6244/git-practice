133f7cf6cde6c60a5b6064a2677eade5c45b3a86
priya <haripriya.godishala12@gmail.com>
2024-08-31 13:46:06 +0530

    files

--- 12-functions.sh
+++ 12-functions.sh
@@ -5,15 +5,15 @@ USERID=$(id -u)
 echo "user id is: $USERID"
 
 VALIDATE(){
-    if [ $1 != 0 ]
-    then
     echo "exit status: $1"
-    fi
 }
+
 if [ $USERID != 0 ]
 then
     echo "Please run this script with root privileges"
     exit 1
 fi
+
+dnf list installed git
 
 VALIDATE $?

cb4fde62a9a3197981c00186342d354652dd61d9
priya <haripriya.godishala12@gmail.com>
2024-08-31 14:00:49 +0530

    files

--- 12-functions.sh
+++ 12-functions.sh
@@ -5,7 +5,13 @@ USERID=$(id -u)
 echo "user id is: $USERID"
 
 VALIDATE(){
-    echo "exit status: $1"
+    if [ $1 != 0 ]
+    then
+        echo "Not yet installed, Going to install it.."
+    else
+        echo "Installed"
+    fi
+    
 }
 
 if [ $USERID != 0 ]
@@ -17,3 +23,18 @@ fi
 dnf list installed git
 
 VALIDATE $?
+
+# if [ $? != 0 ]
+# then
+#     echo "Not yet installed, Going to install it.."
+#     dnf install mysql -y
+#     if [ $? != 0 ]
+#     then
+#         echo "Not successful, pls check"
+#         exit 1    
+#     else
+#         echo "Installation is successful"
+#     fi
+# else
+#     echo "Already Installed"  
+# fi

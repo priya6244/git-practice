655175e2289577885ba93e2e2ae9302b5d5ac467
priya <haripriya.godishala12@gmail.com>
2024-08-31 17:07:30 +0530

    files

--- 15-redirectors.sh
+++ 15-redirectors.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 
-# LOG_FOLDER="/var/log/shell-script"
-# SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
-# TIME=$(date +%Y-%m-%d-%H-%M-%S)
-# LOG_FILE="$LOG_FOLDER/$SCRIPT_NAME-$TIME.log"
-# mkdir -p $LOG_FOLDER
+LOG_FOLDER="/var/log/shell-script"
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+TIME=$(date +%Y-%m-%d-%H-%M-%S)
+LOG_FILE="$LOG_FOLDER/$SCRIPT_NAME-$TIME.log"
+mkdir -p $LOG_FOLDER
 
 USERID=$(id -u)
 
@@ -16,21 +16,22 @@ N="\e[0m"
 VALIDATE(){
     if [ $? != 0 ]
     then
-        echo -e "$R $PACKAGE is not installed, going to install it.. $N"
+        echo -e "$R $PACKAGE is not installed, going to install it.. $N" &>> LOG_FILE
         dnf install $PACKAGE -y
     else
-        echo -e "$G $PACKAGE is already Installed, nothing to do.. $N"
+        echo -e "$G $PACKAGE is already Installed, nothing to do.. $N" &>> LOG_FILE
+        dnf install $PACKAGE -y
     fi   
 }
 
 USAGE(){
-    echo -e "$R It should be like sh 15-redirectors.sh package1 package2 .. $N "
+    echo -e "$R It should be like sh 15-redirectors.sh package1 package2 .. $N " &>> LOG_FILE
     exit 1
 }
 
 if [ $USERID != 0 ]
 then
-    echo -e "$R Please run this script with root privileges $N"
+    echo -e "$R Please run this script with root privileges $N" &>> LOG_FILE
     exit 1
 fi
 
@@ -44,11 +45,7 @@ fi
 for PACKAGE in $@ # 1st time git
 do
     dnf list installed $PACKAGE # git installed, exit status 0
-    # if [ $? != 0 ]
-    # then
-    #     echo "$PACKAGE is not installed, going to install it"
-    #     dnf install $PACKAGE -y
+    
     VALIDATE $? "Installing $PACKAGE"
-    # else
-    #     echo "$PACKAGE is already installed"
+    
 done

4edc28d26e65a77eb30472f28217e41259579b1f
priya <haripriya.godishala12@gmail.com>
2024-09-02 16:52:30 +0530

    Deleting old logs

---
+++ 18-Deleting-logs.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+$SOURCE_DIR="home/ec2-user/files"
+
+if [ -d $SOURCE_DIR ]
+then
+    echo "$SOURCE_DIR exists"
+else
+    echo "$SOURCE_DIR does not exists"
+fi
+
+# FILES=$(find ${SOURCE_DIR} -name "*.log" -mtime +14)
+# echo "files: $FILES"
+
+
+

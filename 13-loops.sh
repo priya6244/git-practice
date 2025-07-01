df49b176924e5bfad63f91578ccfca284decefa3
priya <haripriya.godishala12@gmail.com>
2025-07-01 13:05:36 +0530

    Loops

--- 13-loops.sh
+++ 13-loops.sh
@@ -2,14 +2,14 @@
 
 #writing script in for loop
 
-for i in {1..10}
-do
-    echo "$i"
-done
-
-# for PACKAGE in $@
+# for i in {1..10}
 # do
-#     echo $@
+#     echo "$i"
 # done
+
+for PACKAGE in $@
+do
+    echo $@
+done
 
 #echo " $0 | cut -d "." -f1 "

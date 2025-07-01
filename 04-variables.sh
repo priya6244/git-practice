2413b587408038171a33f5db9fa40e2b1a69c0a7
priya <haripriya.godishala12@gmail.com>
2025-07-01 11:43:28 +0530

    Variable passing through arguments args

--- 04-variables.sh
+++ 04-variables.sh
@@ -5,5 +5,5 @@ person2=$2
 
 echo "$person1: hi $person2"
 echo "${person2}: hi ${person1}, how r u?"
-echo "$person1: iam fine, what abt u?"
+echo "$person1: iam fine, what abt u $person2?"
 echo "${person2}: iam gd."

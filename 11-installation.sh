c6b5176e8d5fa5194d2b110efac90e0330900aee
priya <haripriya.godishala12@gmail.com>
2025-07-01 12:19:48 +0530

    Mysql Installation checking

--- 11-installation.sh
+++ 11-installation.sh
@@ -11,27 +11,27 @@ echo "user id is: $USERID"
 #     echo "you are not the root user"
 # fi
 
-if [ $USERID != 0 ]
+if [ $USERID != 0 ] #checks userID not equals to 0 or not
 then
-    echo "Please run this script with root privileges"
+    echo "Please run this script with root privileges" # print this if userId notEquals to 0
     exit 1
 fi
 #PACKAGE=$1
 
 #dnf install git -y
-dnf list installed mysql
+dnf list installed mysqll #lists the mysql if it is already installed
 
-if [ $? == 0 ]
+if [ $? == 0 ]           # $? gives the exit status of the previous command, here it gives the exit status of the dnf list installed mysql command
 then
-    echo "Already Installed"
+    echo "Already Installed"   # print this if S? == 0, means success -> mysql already installed
 else
-    echo "Not yet installed, Going to install it.."
-    dnf install mysql -y
-    if [ $? == 0 ]
+    echo "Not yet installed, Going to install it.." # print this if S? != 0, means failure -> mysql not installed yet
+    dnf install mysql -y       # install mysql
+    if [ $? == 0 ]        # $? gives the exit status of the previous command, here it gives the exit status of the dnf install mysql command
     then
-        echo "Installation is successful"
+        echo "Installation is successful" # print this if $? = 0, means success
     else
-        echo "Not successful, pls check"
+        echo "Not successful, pls check"  # print this if $? != 0, means failure
         exit 1
     fi   
 fi

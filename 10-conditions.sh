40fb761a7144a54f93f3b4444bda43e2268f66b7
priya <haripriya.godishala12@gmail.com>
2025-07-01 12:16:17 +0530

    Conditions

--- 10-conditions.sh
+++ 10-conditions.sh
@@ -2,21 +2,18 @@
 
 #script to know the entered number is greater than 20 or not
 
-# NUMBER=$1
-# if [ $NUMBER -gt 20 ]
-# then
-#     echo "$NUMBER is greater than 20"
-# else
-#     echo "$NUMBER is less than 20"
-# fi
-
-#write a script to know the given number is greater than or equal to 20
-
 NUMBER=$1
-if [ $NUMBER -ge 20 ]
+if [ $NUMBER -gt 20 ]
 then
-    echo "$NUMBER is greater than or equals to 20"
+    echo "$NUMBER is greater than 20"
 else
-    echo "$NUMBER is less than 20"
+    if [ $NUMBER == 20 ]
+    then
+        echo "$NUMBER is equals to 20"
+    else
+        echo "$NUMBER is less than 20"
+    fi
 fi
+
+
 

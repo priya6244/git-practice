b18ef7feb9a7ee9addaf0ae653c760af7ee4d2a2
priya <haripriya.godishala12@gmail.com>
2025-07-01 11:38:36 +0530

    Variable script

--- 04-variables.sh
+++ 04-variables.sh
@@ -4,6 +4,6 @@ person1=$1
 person2=$2
 
 echo "$person1: hi $person2"
-echo "$person2: hi, how r u?"
+echo "${person2}: hi ${person1}, how r u?"
 echo "$person1: iam fine, what abt u?"
-echo "$person2: iam gd."
+echo "${person2}: iam gd."

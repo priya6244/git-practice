6d370c5ecd2ab6e23ba6adb2e5af835f5dcaf541
priya <haripriya.godishala12@gmail.com>
2025-07-01 12:52:12 +0530

    Functions

--- 12-functions.sh
+++ 12-functions.sh
@@ -8,48 +8,86 @@ N="\e[0m"
 
 echo "user id is: $USERID"
 
-VALIDATE(){
-    if [ $1 != 0 ]
-    then
-        echo -e "$R Not yet installed, Going to install it.. $N"
-        dnf install nginx -y
-    else
-        echo -e "$G Already Installed $N"
-    fi  
-}
-
 if [ $USERID != 0 ]
 then
     echo "Please run this script with root privileges"
     exit 1
 fi
 
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo "$2 is...FAILED"
+        exit 1
+    else
+        echo "$2 is...SUCCESS"
+    fi
+}
+
 dnf list installed nginx
 
-VALIDATE $? "Installing nginx"
+if [ $? -ne 0 ]
+then
+    echo "Nginx is not installed, going to install it.."
+    dnf install nginx -y
+    VALIDATE $? "Installing Nginx"
+else
+    echo "Nginx is already installed, nothing to do.."
+fi
 
+dnf list installed mysql
 
+if [ $? -ne 0 ]
+then
+    echo "MySQL is not installed...going to install"
+    dnf install mysql -y
+    VALIDATE $? "Installing MySQL"
+else
+    echo "MySQL is already installed..nothing to do"
+fi
 
-# if [ $? -ne 0 ]
-# then
-#     echo "nginx is not installed...going to install"
-#     dnf install nginx -y
-#     VALIDATE $?
-# else
-#     echo "MySQL is already installed..nothing to do"
-# fi
-
-# if [ $? != 0 ]
-# then
-#     echo "Not yet installed, Going to install it.."
-#     dnf install mysql -y
-#     if [ $? != 0 ]
+# VALIDATE(){
+#     if [ $1 != 0 ]
 #     then
-#         echo "Not successful, pls check"
-#         exit 1    
+#         echo -e "$R Not yet installed, Going to install it.. $N"
+#         dnf install nginx -y
 #     else
-#         echo "Installation is successful"
-#     fi
-# else
-#     echo "Already Installed"  
+#         echo -e "$G Already Installed $N"
+#     fi  
+# }
+
+# if [ $USERID != 0 ]
+# then
+#     echo "Please run this script with root privileges"
+#     exit 1
 # fi
+
+# dnf list installed nginx
+
+# VALIDATE $? "Installing nginx"
+
+
+
+# # if [ $? -ne 0 ]
+# # then
+# #     echo "nginx is not installed...going to install"
+# #     dnf install nginx -y
+# #     VALIDATE $?
+# # else
+# #     echo "MySQL is already installed..nothing to do"
+# # fi
+
+# # if [ $? != 0 ]
+# # then
+# #     echo "Not yet installed, Going to install it.."
+# #     dnf install mysql -y
+# #     if [ $? != 0 ]
+# #     then
+# #         echo "Not successful, pls check"
+# #         exit 1    
+# #     else
+# #         echo "Installation is successful"
+# #     fi
+# # else
+# #     echo "Already Installed"  
+# # fi

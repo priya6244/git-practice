4473b31381d3c8eefdc22d85b722059ec5d38646
priya <haripriya.godishala12@gmail.com>
2025-07-01 11:28:21 +0530

    Coversation script

--- 02-conversations.sh
+++ 02-conversations.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-ramesh: hi
-suresh: hi, how r u?
-ramesh: iam fine, what abt u?
-suresh: iam gd.
+ramesh: "hi"
+suresh: "hi, how r u?"
+ramesh: "iam fine, what abt u?"
+suresh: "iam gd."

ef164897d0a9da1413a78507a10dedaa94f4ad1a
priya <haripriya.godishala12@gmail.com>
2025-07-01 13:16:29 +0530

    Loops

--- 14-loops.sh
+++ 14-loops.sh
@@ -8,33 +8,33 @@ N="\e[0m"
 
 echo "user id is: $USERID"
 
-VALIDATE(){
-    if [ $1 != 0 ]
-    then
-        echo -e "$R Not yet installed, Going to install it.. $N"
-    else
-        echo -e "$G Installed $N"
-    fi   
-}
-
 if [ $USERID != 0 ]
 then
     echo "Please run this script with root privileges"
     exit 1
 fi
 
-VALIDATE $?
+VALIDATE(){
+    if [ $1 != 0 ]
+    then
+        echo -e "$2 is $R failed $N"
+        exit 1
+    else
+        echo -e "$2 is $G success $N"
+    fi   
+}
+
+#package=mysql
 
 for PACKAGE in $@
 do
     dnf list installed $PACKAGE
     if [ $? != 0 ]
     then
-        echo "$PACKAGE is noyt installed, going to install it"
+        echo "$PACKAGE is not installed, going to install it"
         dnf install $PACKAGE -y
-        VALIDATE $? "Installing $PACKAGE
+        VALIDATE $? "Installing $PACKAGE"
     else
         echo "$PACKAGE is already installed"
-    dnf install $PACKAGE -y
-
+    fi
 done
